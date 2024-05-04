b79966587202c84b5d647b61bb7eadc7e987a125
MasouShinku <2389837103@qq.com>
2024-05-05 03:44:20 +0800

    update test case

--- blockchain/main.go
+++ blockchain/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"blockchain/blockchain"
-	"blockchain/trade"
-	"fmt"
+	"blockchain/service"
 )
 
 //// Block 区块结构体
@@ -40,81 +38,94 @@ func main() {
 	//
 	//}
 
-	txPool := make([]*trade.Trade, 0)
-	var tempTx *trade.Trade
-	var ok bool
-	var property int
-	chain := blockchain.InitBlockChain()
-	property, _ = chain.FindUTXOs([]byte("Leo Cao"))
-	fmt.Println("Balance of Leo Cao: ", property)
+	//txPool := make([]*trade.Trade, 0)
+	//var tempTx *trade.Trade
+	//var ok bool
+	//var property int
+	//chain := blockchain.InitBlockChain()
+	//property, _ = chain.FindUTXOs([]byte("Leo Cao"))
+	//fmt.Println("Balance of Leo Cao: ", property)
+	//
+	//tempTx, ok = chain.CreateTrade([]byte("Leo Cao"), []byte("Krad"), 100)
+	//if ok {
+	//	txPool = append(txPool, tempTx)
+	//}
+	//chain.Mine(txPool)
+	//txPool = make([]*trade.Trade, 0)
+	//property, _ = chain.FindUTXOs([]byte("Leo Cao"))
+	//fmt.Println("Balance of Leo Cao: ", property)
+	//
+	//tempTx, ok = chain.CreateTrade([]byte("Krad"), []byte("Exia"), 200) // this transaction is invalid
+	//if ok {
+	//	txPool = append(txPool, tempTx)
+	//}
+	//
+	//tempTx, ok = chain.CreateTrade([]byte("Krad"), []byte("Exia"), 50)
+	//if ok {
+	//	txPool = append(txPool, tempTx)
+	//}
+	//
+	//tempTx, ok = chain.CreateTrade([]byte("Leo Cao"), []byte("Exia"), 100)
+	//if ok {
+	//	txPool = append(txPool, tempTx)
+	//}
+	//chain.Mine(txPool)
+	//txPool = make([]*trade.Trade, 0)
+	//property, _ = chain.FindUTXOs([]byte("Leo Cao"))
+	//fmt.Println("Balance of Leo Cao: ", property)
+	//property, _ = chain.FindUTXOs([]byte("Krad"))
+	//fmt.Println("Balance of Krad: ", property)
+	//property, _ = chain.FindUTXOs([]byte("Exia"))
+	//fmt.Println("Balance of Exia: ", property)
+	//
+	//for _, block := range chain.BlockList {
+	//	fmt.Printf("Timestamp: %d\n", block.Time.Format("2006-01-02 15:04:05"))
+	//	fmt.Printf("hash: %x\n", block.Hash)
+	//	fmt.Printf("Previous hash: %x\n", block.PrevHash)
+	//	fmt.Printf("nonce: %d\n", block.Nonce)
+	//	fmt.Println("Proof of Work validation:", block.ValidatePoW())
+	//}
+	//
+	////I want to show the bug at this version.
+	//
+	//tempTx, ok = chain.CreateTrade([]byte("Krad"), []byte("Exia"), 30)
+	//if ok {
+	//	txPool = append(txPool, tempTx)
+	//}
+	//
+	//tempTx, ok = chain.CreateTrade([]byte("Krad"), []byte("Leo Cao"), 30)
+	//if ok {
+	//	txPool = append(txPool, tempTx)
+	//}
+	//
+	//chain.Mine(txPool)
+	//txPool = make([]*trade.Trade, 0)
+	//
+	//for _, block := range chain.BlockList {
+	//	fmt.Printf("Timestamp: %d\n", block.Time.Format("2006-01-02 15:04:05"))
+	//	fmt.Printf("hash: %x\n", block.Hash)
+	//	fmt.Printf("Previous hash: %x\n", block.PrevHash)
+	//	fmt.Printf("nonce: %d\n", block.Nonce)
+	//	fmt.Println("Proof of Work validation:", block.ValidatePoW())
+	//}
+	//
+	//property, _ = chain.FindUTXOs([]byte("Leo Cao"))
+	//fmt.Println("Balance of Leo Cao: ", property)
+	//property, _ = chain.FindUTXOs([]byte("Krad"))
+	//fmt.Println("Balance of Krad: ", property)
+	//property, _ = chain.FindUTXOs([]byte("Exia"))
+	//fmt.Println("Balance of Exia: ", property)
 
-	tempTx, ok = chain.CreateTransaction([]byte("Leo Cao"), []byte("Krad"), 100)
-	if ok {
-		txPool = append(txPool, tempTx)
-	}
-	chain.Mine(txPool)
-	txPool = make([]*trade.Trade, 0)
-	property, _ = chain.FindUTXOs([]byte("Leo Cao"))
-	fmt.Println("Balance of Leo Cao: ", property)
+	service := service.Service{}
+	service.CreateBlockChain("LeoCao")
+	service.Balance("LeoCao")
+	service.GetBlockChainInfo()
+	service.Send("Leo", "Cao", 100)
+	service.Send("LeoCao", "Krad", 100)
+	service.Balance("LeoCao")
+	service.Mine()
+	service.Balance("LeoCao")
+	service.Balance("Krad")
+	service.GetBlockChainInfo()
 
-	tempTx, ok = chain.CreateTransaction([]byte("Krad"), []byte("Exia"), 200) // this transaction is invalid
-	if ok {
-		txPool = append(txPool, tempTx)
-	}
-
-	tempTx, ok = chain.CreateTransaction([]byte("Krad"), []byte("Exia"), 50)
-	if ok {
-		txPool = append(txPool, tempTx)
-	}
-
-	tempTx, ok = chain.CreateTransaction([]byte("Leo Cao"), []byte("Exia"), 100)
-	if ok {
-		txPool = append(txPool, tempTx)
-	}
-	chain.Mine(txPool)
-	txPool = make([]*trade.Trade, 0)
-	property, _ = chain.FindUTXOs([]byte("Leo Cao"))
-	fmt.Println("Balance of Leo Cao: ", property)
-	property, _ = chain.FindUTXOs([]byte("Krad"))
-	fmt.Println("Balance of Krad: ", property)
-	property, _ = chain.FindUTXOs([]byte("Exia"))
-	fmt.Println("Balance of Exia: ", property)
-
-	for _, block := range chain.BlockList {
-		fmt.Printf("Timestamp: %d\n", block.Time.Format("2006-01-02 15:04:05"))
-		fmt.Printf("hash: %x\n", block.Hash)
-		fmt.Printf("Previous hash: %x\n", block.PrevHash)
-		fmt.Printf("nonce: %d\n", block.Nonce)
-		fmt.Println("Proof of Work validation:", block.ValidatePoW())
-	}
-
-	//I want to show the bug at this version.
-
-	tempTx, ok = chain.CreateTransaction([]byte("Krad"), []byte("Exia"), 30)
-	if ok {
-		txPool = append(txPool, tempTx)
-	}
-
-	tempTx, ok = chain.CreateTransaction([]byte("Krad"), []byte("Leo Cao"), 30)
-	if ok {
-		txPool = append(txPool, tempTx)
-	}
-
-	chain.Mine(txPool)
-	txPool = make([]*trade.Trade, 0)
-
-	for _, block := range chain.BlockList {
-		fmt.Printf("Timestamp: %d\n", block.Time.Format("2006-01-02 15:04:05"))
-		fmt.Printf("hash: %x\n", block.Hash)
-		fmt.Printf("Previous hash: %x\n", block.PrevHash)
-		fmt.Printf("nonce: %d\n", block.Nonce)
-		fmt.Println("Proof of Work validation:", block.ValidatePoW())
-	}
-
-	property, _ = chain.FindUTXOs([]byte("Leo Cao"))
-	fmt.Println("Balance of Leo Cao: ", property)
-	property, _ = chain.FindUTXOs([]byte("Krad"))
-	fmt.Println("Balance of Krad: ", property)
-	property, _ = chain.FindUTXOs([]byte("Exia"))
-	fmt.Println("Balance of Exia: ", property)
 }
