dc9a5dc873c0b4f94950a3056a026ea30b6fd477
MasouShinku <2389837103@qq.com>
2024-05-05 13:58:50 +0800

    add util.log

--- blockchain/util/util.go
+++ blockchain/util/util.go
@@ -1,16 +1,41 @@
 package util
 
-import "os"
+import (
+	"log"
+	"os"
+	"sync"
+)
 
+// blockchain相关
 const (
 	Difficulty = 12
 	InitNum    = 1000
-	TradePool  = "./files/tradePool.data"  //this line is new
-	BCPath     = "./files/blocks"          //this line is new
-	BCFile     = "./files/blocks/MANIFEST" //this line is new
+	TradePool  = "./files/tradePool.data"
+	BCPath     = "./files/blocks"
+	BCFile     = "./files/blocks/MANIFEST"
 )
 
-// 检查文件是否存在
+// 日志相关
+const (
+	preError   = "\033[31m[Error]\033[0m"
+	preInfo    = "\033[34m[Info]\033[0m"
+	InfoLevel  = 0
+	ErrorLevel = 1
+)
+
+var (
+	errorLog = log.New(os.Stdout, preError, log.LstdFlags|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, preInfo, log.LstdFlags|log.Lshortfile)
+	loggers  = []*log.Logger{errorLog, infoLog}
+	mu       sync.Mutex
+)
+
+// 暴露log方法
+var (
+	Errorln = errorLog.Println
+	Infoln  = infoLog.Println
+)
+
 func FileExists(fileAddr string) bool {
 	if _, err := os.Stat(fileAddr); os.IsNotExist(err) {
 		return false
